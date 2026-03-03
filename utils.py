185ec0a4e6ff1c91e8793dd752456678101d1f43
adietya89 <aplikasikemenkes89@gmail.com>
2026-03-03 11:49:13 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
+
+features = ["Open", "High", "Low", "Close", "Volume"]
 
 def prepare_data(saham):
     df = yf.download(saham + ".JK", period="3mo", progress=False)
@@ -9,3 +12,10 @@ def prepare_data(saham):
     df["MA50"] = df["Close"].rolling(50).mean()
     df = df.dropna()
     return df
+
+class DummyModel:
+    def predict_proba(self, X):
+        return [[0.3, 0.7]]
+
+model_xgb = DummyModel()
+model_lstm = DummyModel()
